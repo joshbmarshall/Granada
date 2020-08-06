882d78ea5c17c476b1896fc5d913c0774dbb1dcb
Josh Marshall <josh@jmarshall.com.au>
2020-08-07 00:20:39 +1000

    adjust time zone WIP

--- src/Granada/ExtendedModel.php
+++ src/Granada/ExtendedModel.php
@@ -14,4 +14,38 @@ class ExtendedModel extends Model {
         }
         return parent::__get($property);
     }
+
+    /**
+      * Check if a date/time field needs timezone adjustment for use in queries
+      *
+      * @param string $property
+      * @param \Cake\Chronos\Chronos $value
+      * @return string
+      */
+    public static function adjustTimezoneForWhere($property, $value) {
+        $class = get_called_class();
+        $datefields = $class::datefields();
+        if (!array_key_exists($property, $datefields)) {
+            $property = (new $class)->tablename() . '.' . $property;
+        }
+        if (array_key_exists($property, $datefields)) {
+            if ($datefields[$property]['type'] == 'date') {
+                $timezone = $value->timezone; // No change for date
+            } else if ($datefields[$property]['timezone_comparison_mode'] == 'user') {
+                $timezone = self::currentTimezone();
+            } else if ($datefields[$property]['timezone_comparison_mode'] == 'site') {
+                $timezone = self::siteTimezone();
+            } else if ($datefields[$property]['timezone_mode'] == 'none') {
+                $timezone = $value->timezone; // No timezone, time is time of day regardless of the time zone checking against
+            } else {
+                $timezone = 'Etc/UTC';
+            }
+
+            $format = $datefields[$property]['format'];
+
+            $value = $value->timezone($timezone)->format($format);
+        }
+
+        return $value;
+    }
 }
