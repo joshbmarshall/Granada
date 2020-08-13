89581e4aa6e0cc746186179e10fdc22da5db5c19
Josh Marshall <josh@jmarshall.com.au>
2020-08-13 23:00:29 +1000

    Information on how to configure db2model

--- tests/testdb2modelconf.php
+++ tests/testdb2modelconf.php
@@ -1,15 +1,12 @@
-#!/usr/bin/env php
 <?php
-
-// Configuration defaults
-$db_host = 'database';
-$db_host='database';
-$db_name='testdb';
-$db_username='root';
-$db_password="root";
-$models_output_dir = __DIR__ . '/Auto';
-$model_to_extend = '\MyAppTest\ORMBaseClass';
+$db_host                    = 'database';
+$db_host                    = 'database';
+$db_name                    = 'testdb';
+$db_username                = 'root';
+$db_password                = 'root';
+$models_output_dir          = __DIR__ . '/Auto';
+$model_to_extend            = '\MyAppTest\ORMBaseClass';
 $controller_model_to_extend = '\MyAppTest\Controller';
-$use_namespaces = false;
-$default_namespace = 'MyAppTest';
-$plural_tables = [];
+$use_namespaces             = false;
+$default_namespace          = 'MyAppTest';
+$plural_tables              = [];
